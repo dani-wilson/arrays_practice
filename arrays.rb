9a83f1b7e7494fe83bc78375f9b839009059143c
Dani Wilson <danirae.powell@gmail.com>
2023-03-28 15:14:51 -0600

    Add impacts of calling methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,17 @@ favorite_bands = ["Dance Gavin Dance", "Switchfoot", "Needtobreathe", "Mat Kearn
 random_numbers = [12, 21, 19, 8000, 4672]
 bank_balances = [1700.91, 7477.80, 726.01, 70009.71, 590.21]
 true_or_false = [false, false, true, true, false]
+
+#Using .pop will remove the last element of this array.
+favorite_bands.pop
+
+#Using .push will push the given element to the end of the array
+random_numbers.push [21]
+
+#Using .shift will remove an element in an array from first to last
+bank_balances.shift
+
+#Using .unshift is used to fill an array with elements
+# that will begin at the front of the array
+true_or_false.unshift[true]
+
