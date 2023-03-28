f5a98112a083ed654f7d9e972648f2d04cd96268
Dani Wilson <danirae.powell@gmail.com>
2023-03-28 15:05:52 -0600

    Initial commit

---
+++ arrays.rb
@@ -0,0 +1,5 @@
+favorite_bands = ["Dance Gavin Dance", "Switchfoot", "Needtobreathe", "Elliot Root", "Stop Light Observations"]
+random_numbers = [7, 18, 2400, 12, 91, 438]
+account_balances = [1400.00, 71.55, 800.87, 100000.66, 409.25]
+true_or_false = [true, false, false, true, false]
+
