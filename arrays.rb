168ce281c9010e0b5e434b8bff6c154492c3d124
Dani Wilson <danirae.powell@gmail.com>
2023-03-28 15:08:58 -0600

    Add arrays

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+favorite_bands = ["Dance Gavin Dance", "Switchfoot", "Needtobreathe", "Mat Kearney", "Elliot Root", "Stop Light Observations"]
+random_numbers = [12, 21, 19, 8000, 4672]
+bank_balances = [1700.91, 7477.80, 726.01, 70009.71, 590.21]
+true_or_false = [false, false, true, true, false]
